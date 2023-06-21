452160f1e442970bff1b5ac672f912d3fbb9d3e8
Malcom Rojas <malcomaep@gmail.com>
2023-06-20 20:23:53 -0400

    inverted logo

--- game-hub/src/components/Navbar.tsx
+++ game-hub/src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import "./index.css";
 
 import logo from "../assets/MLCM RADIO NEW LOGO.png";
 
 const Navbar = () => {
   return (
     <HStack>
-      <Image src={logo} boxSize="60px" />
+      <Image id="logo" src={logo} boxSize="60px" />
       <Text>NavBar</Text>
     </HStack>
   );
